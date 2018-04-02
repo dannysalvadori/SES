c3a3632c1c3c4f8f247925f30de26226d62bb316
User <User@10.107.142.209>
2018-04-02 13:02:00 +0100

    Hooked up CSS

--- ses/src/main/java/com/fdmgroup/controller/DemoController.java
+++ ses/src/main/java/com/fdmgroup/controller/DemoController.java
@@ -1,12 +1,21 @@
 package com.fdmgroup.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fdmgroup.entity.User;
+import com.fdmgroup.repository.UserRepository;
+
 @Controller
 public class DemoController {
+	
+	@Autowired
+	private UserRepository userRepo;
 
 	@RequestMapping(value="/")
     public ModelAndView index() {
@@ -15,7 +24,10 @@ public class DemoController {
 	
 	@RequestMapping(value="/hello")
     public String sayHi(Model model) {
-		model.addAttribute("number", 7);
+		User user = new User();
+		userRepo.save(user);
+		List<User> users = userRepo.findAll();
+		model.addAttribute("number", users.size());
         return "hi";
 	}
 	
