4612c512f19d8f23a269e86a7d2614268866c606
User <User@192.168.182.217>
2018-04-22 13:41:51 +0100

    Commented out redundant (maybe?) bean definition

--- ses/src/main/java/com/fdmgroup/ses/SesApplication.java
+++ ses/src/main/java/com/fdmgroup/ses/SesApplication.java
@@ -44,22 +44,22 @@ public class SesApplication extends SpringBootServletInitializer {
 		return null;
     }
 	
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("dannysalvadori@gmail.com");
-	    mailSender.setPassword("Veluae-158703");
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	     
-	    return mailSender;
-	}
-	
+//	@Bean
+//	public JavaMailSender getJavaMailSender() {
+//	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//	    mailSender.setHost("smtp.gmail.com");
+//	    mailSender.setPort(587);
+//	     
+//	    mailSender.setUsername("dannysalvadori@gmail.com");
+//	    mailSender.setPassword("Veluae-158703");
+//	     
+//	    Properties props = mailSender.getJavaMailProperties();
+//	    props.put("mail.transport.protocol", "smtp");
+//	    props.put("mail.smtp.auth", "true");
+//	    props.put("mail.smtp.starttls.enable", "true");
+//	    props.put("mail.debug", "true");
+//	     
+//	    return mailSender;
+//	}
+//	
 }
