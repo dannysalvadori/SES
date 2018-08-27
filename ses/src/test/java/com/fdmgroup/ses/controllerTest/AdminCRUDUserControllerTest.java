be0328f2b7b2890735fb6fae07069238ce23e1d8
User <User@192.168.1.34>
2018-08-27 13:53:21 +0100

    Fixed up admin CRUD controller tests

--- ses/src/test/java/com/fdmgroup/ses/controllerTest/AdminCRUDUserControllerTest.java
+++ ses/src/test/java/com/fdmgroup/ses/controllerTest/AdminCRUDUserControllerTest.java
@@ -1,22 +1,26 @@
 package com.fdmgroup.ses.controllerTest;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
-import javax.transaction.Transactional;
+import static com.fdmgroup.ses.utils.UserUtils.*;
+import static com.fdmgroup.ses.utils.RoleUtils.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,61 +31,55 @@ import com.fdmgroup.ses.repository.RoleRepository;
 import com.fdmgroup.ses.repository.UserRepository;
 import com.fdmgroup.ses.service.UserService;
 import com.fdmgroup.ses.validation.SesValidationException;
-import com.fdmgroup.ses.validation.ValidationUtils;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
-@Transactional // Allows detached entities (caused by multiple entityManager instances) to be persisted
 public class AdminCRUDUserControllerTest {
 	
-	private static final int STATUS_ACTIVE = 1;
-	private static final int STATUS_INACTIVE = 0;
-
-	@Autowired
-	private AdminCRUDUserController ctrl;
-
-	@Autowired
-	RoleRepository roleRepo;
-
-	@Autowired
-	UserRepository userRepo;
-
-	@Autowired
-	UserService userService;
+	@InjectMocks
+	private AdminCRUDUserController ctrl = new AdminCRUDUserController();
+	private ModelAndView mav = new ModelAndView();
 
 	@Mock
-	WebRequest webRequest;
+	private RoleRepository roleRepo;
 
-	private ModelAndView mav = new ModelAndView();
-	private final String VALID_TEST_EMAIL = String.valueOf(Math.random()) + "@sesTest.com";
-	private final String ALTERNATE_VALID_TEST_EMAIL = String.valueOf(Math.random()) + "@sesTest2.com";
+	@Mock
+	private UserRepository userRepo;
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private WebRequest webRequest;
+	
+	@Mock
+	WebDataBinder binder;
+	
+	private List<User> allUsers = new ArrayList<>();
+	private List<Role> allRoles = new ArrayList<>();
 
 	@Before
 	public void setUp() throws Exception {
-		User testUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertEquals("A user already exists with test email 1. Consider re-running test", null, testUser);
-		testUser = userRepo.findByEmail(ALTERNATE_VALID_TEST_EMAIL);
-		assertEquals("A user already exists with test email 2. Consider re-running test", null, testUser);
+		setupAllUsers();
+		setupAllRoles();
+		when(userRepo.findAll()).thenReturn(allUsers);
+		when(roleRepo.findAll()).thenReturn(allRoles);
 	}
-
-	/**
-	 * Delete any DB data persisted during the test
-	 */
-	@After
-	public void revert() {
-		User testUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		if (testUser != null) {
-			userService.deleteUser(testUser);
-		}
-		testUser = userRepo.findByEmail(ALTERNATE_VALID_TEST_EMAIL);
-		if (testUser != null) {
-			userService.deleteUser(testUser);
+	
+	private void setupAllUsers() {
+		for (Integer i = 0; i < 3; i++) {
+			User u = createUser();
+			u.setEmail(u.getEmail() + i);
+			u.setId(i);
+			u.setActive(1);
+			allUsers.add(u);
 		}
 	}
-
-	/*******************************************
-	 * Tests Start *
-	 *******************************************/
+	
+	private void setupAllRoles() {
+		allRoles.add(ROLE_USER);
+		allRoles.add(ROLE_ADMIN);
+	}
 
 	/**
 	 * manageUsers() adds all users to model (as "users") and sets the view to
@@ -92,10 +90,10 @@ public class AdminCRUDUserControllerTest {
 		mav = ctrl.goToManageUsers(mav);
 		assertEquals("Wrong view name", "admin/manageUsers", mav.getViewName());
 		Object usersModelObject = mav.getModel().get("users");
-		assertNotEquals(null, usersModelObject);
-		assertTrue("newUser is wrong type", usersModelObject instanceof Collection<?>);
+		assertNotEquals("allUsers model object was null", null, usersModelObject);
+		assertTrue("allUsers model object is wrong type", usersModelObject instanceof Collection<?>);
 		Collection<?> users = (Collection<?>) usersModelObject;
-		assertEquals("Didn't find all users", users.size(), userRepo.findAll().size());
+		assertEquals("Didn't find all users", users.size(), allUsers.size());
 		for (Object obj : users) {
 			assertTrue("'User' is wrong type", obj instanceof User);
 		}
@@ -114,122 +112,67 @@ public class AdminCRUDUserControllerTest {
 	}
 
 	/**
-	 * If validation succeeds, createUser() inserts the given User object into the
-	 * DB
+	 * If validation succeeds, createUser() inserts the given User object
 	 */
 	@Test
-	public void doCreateUserSuccessTest() {
-		User testUser = createUser();
-		mav = ctrl.doCreateUser(mav, testUser, webRequest);
+	public void doCreateUserSuccessTest() throws SesValidationException {
+		User u = createUser();
+		mav = ctrl.doCreateUser(mav, u, webRequest);
 
-		// Confirm validation passes
+		// Confirm validation passes, saveUser(u) is called, and view is set back to manage users page
 		Object failures = mav.getModel().get("failures");
 		assertEquals("Validation failed", null, (String) failures);
-
-		User resultUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertTrue("Null user", resultUser != null);
-		assertEquals("Wrong email", VALID_TEST_EMAIL, resultUser.getEmail());
-		assertEquals("Wrong amount of credit", new BigDecimal(50000), resultUser.getCredit());
+		verify(userService, times(1)).saveUser(u, webRequest);
+		assertEquals("Wrong view name", "admin/manageUsers", mav.getViewName());
 	}
 
 	/**
-	 * If the user's password doesn't match the confirmation password, createUser()
-	 * must not persist the given User object.
+	 * If the user fails validation, createUser() does not persist the object and reports failures
+	 * @throws SesValidationException 
 	 */
 	@Test
-	public void doCreateUserPasswordMismatchTest() {
-		User testUser = createUser();
-		testUser.setConfirmationPassword("Something different");
-		mav = ctrl.doCreateUser(mav, testUser, webRequest);
+	public void doCreateUserFailureTest() throws SesValidationException {
+		User u = createUser();
+		final String VALIDATION_FAILURE = "VALIDATION_FAILURE";
+		SesValidationException vEx = new SesValidationException();
+		vEx.addFailure(VALIDATION_FAILURE);
+		doThrow(vEx).when(userService).saveUser(u, webRequest); // How vExing, har har...
+		mav = ctrl.doCreateUser(mav, u, webRequest);
 
 		// Confirm validation failure
 		Object failures = mav.getModel().get("failures");
-		assertTrue("Validation didn't fail", ((String) failures).equals("Passwords do not match."));
-
-		User resultUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertTrue("Null user", resultUser == null);
+		assertTrue("Validation didn't fail", ((String) failures).equals(VALIDATION_FAILURE));
+		assertEquals("Wrong view name", "admin/createUser", mav.getViewName());
 	}
 
 	/**
-	 * If the user's password is too short, createUser() must not persist the given
-	 * User object.
-	 */
-	@Test
-	public void doCreateUserPasswordTooShortTest() {
-		User testUser = createUser();
-		testUser.setPassword("A");
-		testUser.setConfirmationPassword("A");
-		mav = ctrl.doCreateUser(mav, testUser, webRequest);
-
-		Object failures = mav.getModel().get("failures");
-		assertTrue("Validation didn't fail", ((String) failures).equals("Password must be 6 to 50 characters long."));
-
-		User resultUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertTrue("Null user", resultUser == null);
-	}
-
-	/**
-	 * If the user's password is too long, createUser() must not persist the given
-	 * User object.
-	 */
-	@Test
-	public void doCreateUserPasswordTooLongTest() {
-		// Generate a password of 60 chars length
-		String tenChars = "XXXXXXXXXX";
-		String longPassword = "";
-		for (int i = 0; i < 6; i++) {
-			longPassword += tenChars;
-		}
-
-		User testUser = createUser();
-		testUser.setPassword(longPassword);
-		testUser.setConfirmationPassword(longPassword);
-		mav = ctrl.doCreateUser(mav, testUser, webRequest);
-
-		Object failures = mav.getModel().get("failures");
-		assertTrue("Validation didn't fail", ((String) failures).equals("Password must be 6 to 50 characters long."));
-
-		User resultUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertTrue("Null user", resultUser == null);
-	}
-
-	/**
-	 * If the user's email has already been taken, createUser() must not persist the
-	 * given User object.
-	 */
-	@Test
-	public void doCreateUserAlreadyExistsTest() {
-		// Insert user and confirm persistence
-		User testUser = insertTestUser();
-
-		// Attempt to insert another user with the same email
-		testUser = createUser();
-		mav = ctrl.doCreateUser(mav, testUser, webRequest);
-
-		// Confirm validation failure
-		Object failures = mav.getModel().get("failures");
-		assertTrue("Validation didn't fail",
-				((String) failures).equals("A user is already registered with this address."));
-	}
-
-	/**
-	 * toggleActive() sets a user to active or inactive (whichever it wasn't before
-	 * the toggle)
+	 * toggleActive() sets a user to active or inactive (whichever it wasn't before the toggle)
 	 */
 	@Test
 	public void toggleActiveTest() {
-		// Insert user and confirm persistence
-		User testUser = insertTestUser();
+		User u = allUsers.get(0);
+		assertEquals("Test user did not start active!", 1, u.getActive());
+		when(userRepo.findById(u.getId())).thenReturn(u);
 
 		// Attempt to toggle user inactive
-		mav = ctrl.toggleActive(mav, testUser.getId());
-		testUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertEquals("Wrong active status", STATUS_INACTIVE, testUser.getActive());
+		mav = ctrl.toggleActive(mav, u.getId());
+		assertEquals("Wrong active status", 0, u.getActive());
+		verify(userRepo, times(1)).save(u);
+		
+		// Confirm return to manage users view
+		assertEquals("Wrong view name", "admin/manageUsers", mav.getViewName());
+		Collection<?> users = (Collection<?>) mav.getModel().get("users");
+		assertEquals("Didn't find all users", users.size(), allUsers.size());
 
 		// Toggle user active again
-		mav = ctrl.toggleActive(mav, testUser.getId());
-		testUser = userRepo.findByEmail(VALID_TEST_EMAIL);
-		assertEquals("Wrong active status", STATUS_ACTIVE, testUser.getActive());
+		mav = ctrl.toggleActive(mav, u.getId());
+		assertEquals("Wrong active status", 1, u.getActive());
+		verify(userRepo, times(2)).save(u); // Two times because we're counting the earlier verify
+
+		// Confirm return to manage users view
+		assertEquals("Wrong view name", "admin/manageUsers", mav.getViewName());
+		users = (Collection<?>) mav.getModel().get("users");
+		assertEquals("Didn't find all users", users.size(), allUsers.size());
 	}
 
 	/**
@@ -238,120 +181,72 @@ public class AdminCRUDUserControllerTest {
 	 */
 	@Test
 	public void goToEditUserTest() {
-		User testUser = insertTestUser();
-		mav = ctrl.goToEditUser(mav, testUser.getId());
+		User u = allUsers.get(0);
+		when(userRepo.findById(u.getId())).thenReturn(u);
+		
+		// Test
+		mav = ctrl.goToEditUser(mav, u.getId());
+		
 		assertEquals("Wrong view name", "admin/editUser", mav.getViewName());
 		assertNotEquals(null, mav.getModel().get("user"));
 		assertTrue("user is wrong type", mav.getModel().get("user") instanceof User);
 		User modelUser = (User) mav.getModel().get("user");
-		assertEquals("Model User had wrong ID", testUser.getId(), modelUser.getId());
-		assertEquals("Model User had wrong name", testUser.getName(), modelUser.getName());
-		assertEquals("Model User had wrong email", testUser.getEmail(), modelUser.getEmail());
+		assertEquals("Model User had wrong ID", u.getId(), modelUser.getId());
+		assertEquals("Model User had wrong name", u.getName(), modelUser.getName());
+		assertEquals("Model User had wrong email", u.getEmail(), modelUser.getEmail());
+		
+		// Confirm roles were loaded
+		Collection<?> roles = (Collection<?>) mav.getModel().get("allRoles");
+		assertEquals("Didn't find all roles", allRoles.size(), roles.size());
 	}
 
 	/**
 	 * doEditUser() updates the User in the DB, and sets the view back to admin/manageUsers.jsp
 	 */
 	@Test
-	public void doEditUserSuccessTest() {
-		User testUser = insertTestUser();
+	public void doEditUserSuccessTest() throws SesValidationException {
+		User u = allUsers.get(0);
 		
 		// Edit test user's details
-		testUser.setName("ALTERED_NAME");
-		mav = ctrl.doEditUser(mav, webRequest, testUser);
+		u.setName("NEW_NAME");
+		mav = ctrl.doEditUser(mav, webRequest, u);
 		
-		// Get user from DB
-		testUser = userRepo.findByEmail(VALID_TEST_EMAIL);
+		// Confirm user service saved user, user object is reset, and view is reset to manage users
+		verify(userService).saveUser(u, webRequest);
+		assertEquals("User model object was not reset", null, mav.getModel().get("user"));
 		
 		assertEquals("Wrong view name", "admin/manageUsers", mav.getViewName());
-		assertEquals("Model User had wrong ID", "ALTERED_NAME", testUser.getName());
+		Collection<?> users = (Collection<?>) mav.getModel().get("users");
+		assertEquals("Didn't find all users", users.size(), allUsers.size());
 	}
 	
 	/**
 	 * doEditUser() fails if the email is changed to one that is already registered with another user
 	 */
 	@Test
-	@Transactional
-	public void doEditUserEmailAlteredToOneThatIsInUseTest() {
-		User testUser = insertTestUser();
+	public void doEditUserFailureTest() throws SesValidationException {
+		User u = allUsers.get(0);
 		
-		// Insert a second test user with different email
-		User secondUser = createUser();
-		secondUser.setEmail(ALTERNATE_VALID_TEST_EMAIL);
-		secondUser = insertTestUser(secondUser);
+		// Setup validation failure
+		final String VALIDATION_FAILURE = "VALIDATION_FAILURE";
+		SesValidationException vEx = new SesValidationException();
+		vEx.addFailure(VALIDATION_FAILURE);
+		doThrow(vEx).when(userService).saveUser(u, webRequest);
 		
 		// Edit test user's details
-		testUser.setEmail(ALTERNATE_VALID_TEST_EMAIL);
-		mav = ctrl.doEditUser(mav, webRequest, testUser);
+		u.setName("NEW_NAME");
+		mav = ctrl.doEditUser(mav, webRequest, u);
 		
 		// Confirm validation failure
 		Object failures = mav.getModel().get("failures");
-		assertTrue("Validation didn't fail", failures != null);
-		assertTrue("Wrong failures",
-				((String) failures).equals("A user is already registered with this address."));
-	}
-	
-	/**
-	 * doEditUser() fails if the new password is too short
-	 */
-	@Test
-	public void doEditUserPasswordTooShortTest() {
-		// TODO
-	}
-	
-	/**
-	 * doEditUser() fails if the new password is too long
-	 */
-	@Test
-	public void doEditUserPasswordTooLongTest() {
-		// TODO
-	}
-	
-	/**
-	 * doEditUser() fails if the confirmation password doesn't match the new password
-	 */
-	@Test
-	public void doEditUserTheConfirmationPasswordDoesntMatchTest() {
-		// TODO
+		assertTrue("Validation didn't fail", ((String) failures).equals(VALIDATION_FAILURE));
+		assertEquals("Wrong view name", "admin/editUser", mav.getViewName());
+		
+		// Confirm user object was not reset, and view was set back to edit user
+		verify(userService).saveUser(u, webRequest);
+		assertEquals("Wrong view name", "admin/editUser", mav.getViewName());
+		Collection<?> roles = (Collection<?>) mav.getModel().get("allRoles");
+		assertEquals("Didn't find all roles", allRoles.size(), roles.size());
 	}
 
-	/*******************************************
-	 * Helper Methods *
-	 *******************************************/
-
-	private User createUser() {
-		User u = new User();
-		u.setActive(1);
-		u.setEmail(VALID_TEST_EMAIL);
-		u.setPassword("123456");
-		u.setConfirmationPassword("123456");
-		u.setName("Testo");
-		u.setLastName("McTest");
-		Role roleUser = roleRepo.findByRole("ROLE_USER");
-		Set<Role> roles = new HashSet<>();
-		roles.add(roleUser);
-		u.setRoles(roles);
-		return u;
-	}
-
-	private User insertTestUser() {
-		return insertTestUser(null);
-	}
-	private User insertTestUser(User u) {
-		User testUser = u == null ? createUser() : u;
-		try {
-			userService.saveUser(testUser, webRequest);
-		} catch (SesValidationException e) {
-			assertTrue("Test user validation failure: " + ValidationUtils.stringifyFailures(e.getFailures()), false);
-		}
-		// Confirm successful persist
-		User resultUser = userRepo.findByEmail(testUser.getEmail());
-		System.out.println("Ins. id: " + testUser.getId());
-		System.out.println("Ins. email: " + testUser.getEmail());
-		assertTrue("Null user", resultUser != null);
-		assertEquals("Wrong email", testUser.getEmail(), resultUser.getEmail());
-		assertEquals("Wrong active status", STATUS_ACTIVE, resultUser.getActive());
-		return resultUser;
-	}
-	
 }
